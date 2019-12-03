647e11cac3d012452347ed463adca5ac6d3ba04a
Yuki Yoshioka <rikusouda@users.noreply.github.com>
2019-12-03 15:39:28 +0900

    Initial Commit

---
+++ ios-collectionview-selection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-collectionview-selection
+//
+//  Created by yuki.yoshioka on 2019/12/03.
+//  Copyright © 2019 rikusouda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
